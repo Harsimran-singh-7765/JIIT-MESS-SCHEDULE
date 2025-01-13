8216b4558761da9e943403afcc98810b618a1b21
Harsimran Singh <singhharry7765@gmail.com>
2025-01-13 10:18:16 +0530

    Update timetable128.ts

--- src/data/timetable128.ts
+++ src/data/timetable128.ts
@@ -1,6 +1,6 @@
 export const timetable128Data = {
   Monday: 'Arhar Dal, Sahi Paneer, Rice, Chapati, Achar, Salad, Rice Kheer,',
-  Tuesday: 'Makar Sankranti,',
+  
   Wednesday: 'Rajma Dal, Gobhi Kofta, Rice, Chapati, Achar, Salad, Sabudana Kheer,',
   Thursday: 'Chhole, Patta Gobhi Matar, Rice, Bhature, Achar, Salad, Bundi Raita,',
   Friday: 'Dal Makhani, Gobhi Masala, Rice, Chapati, Achar, Salad, Semiya Kheer,',
