d444329f481977ec465ec455d2bf813fed57b01a
Harsimran Singh <singhharry7765@gmail.com>
2025-01-27 18:15:01 +0530

    Update timetable128.ts

--- src/data/timetable128.ts
+++ src/data/timetable128.ts
@@ -1,8 +1,8 @@
 export const timetable128Data = {
   Monday: 'Arhar Dal, Kadahi Paneer, Rice, Chapati, Achar, Salad, Sweet Bundi,',
-  Tuesday: 'Dal Makhani, Mix Veg, Rice, Chapati, Achar, Salad, Gajar Halwa,',
-  Wednesday: 'Noodles, Manchurian, Fried Rice, Tomato Soup, Chilli Sauce, Salad, Bundi Raita,',
-  Thursday: 'Chhole Masale, South Indian Aloo, Rice, Bhature, Achar, Salad, Semiya Kheer,',
-  Friday: 'Chana Dal, Kofta, Rice, Chapati, Achar, Salad, Moong Dal Halwa,',
-  Saturday: 'Karela, Veg Biryani, Nan, Chapati, Achar, Salad, Ladoo,'
+  Tuesday: 'Chana Dal, Mix Veg, Rice, Chapati, Achar, Salad, Gajar Halwa,',
+  Wednesday: 'Noodles, Manchurian, Fried Rice, Tomato Sauce, Salad,',
+  Thursday: 'Chhole Masale, South Indian Aloo, Rice, Bhature, Achar, Salad, Bundi Raita,',
+  Friday: 'Dal Makhani, Kofta, Rice, Chapati, Achar, Salad, Semiya Kheer,',
+  Saturday: 'Karela, Moong Dal, Veg Biryani, Nan, Achar, Salad, Ladoo,'
 };
